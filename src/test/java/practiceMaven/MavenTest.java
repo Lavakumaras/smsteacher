70067e3bda2240ee3f86df2cd2d3223b4c4d00fe
Saransh Dwivedi <Saransh Dwivedi@DESKTOP-0SMTTDJ>
2024-01-24 16:56:26 +0530

    new update with one new class.

---
+++ src/test/java/practiceMaven/MavenTest.java
@@ -0,0 +1,11 @@
+package practiceMaven;
+
+import org.testng.annotations.Test;
+
+public class MavenTest {
+	@Test(groups = {"smoke" , "regression"})
+	public void m3()
+	{
+		System.out.println("2.1");
+	}
+}
